71e03a74556f1a08a99082e41c2a0bb273de73d6
Denis Skvortsov <skvord89@gmail.com>
2020-05-04 11:29:56 +0300

    Add Cli.php with username input

---
+++ src/Cli.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace BrainGames\Cli;
+
+use function cli\line;
+use function cli\prompt;
+
+function run()
+{
+    line('Welcome to  the Brain Games!');
+    line("");
+    $userName = prompt('May I have your name?');
+    line("Hello, {$userName}");
+}
+
