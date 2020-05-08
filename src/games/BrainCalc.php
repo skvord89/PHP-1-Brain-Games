7dd0b3c9bd8ce46237031d0a4bdd9c19146e376f
Denis Skvortsov <skvord89@gmail.com>
2020-05-08 11:40:09 +0300

    Remove gameflow from BrainCalc

--- src/games/BrainCalc.php
+++ src/games/BrainCalc.php
@@ -2,24 +2,23 @@
 
 namespace BrainGames\Games\BrainCalc;
 
-use function cli\line;
-use function cli\prompt;
+use function BrainGames\GameFlow\gameFlow;
 
 function braincalc()
 {
-    line('Welcome to  the Brain Games!');
-    line("");
-    $userName = prompt('May I have your name?');
-    line("Hello, {$userName}\n");
+    $gameSet = generateGameSet();
+    gameFlow($gameSet);
+}
 
-    $winnedRounds = 0;
+function generateGameSet()
+{
+    $gameSet = [];
     $operators = ['+', '-', '*'];
-
-    for ($gameRound = 0; $gameRound < 3; $gameRound += 1) {
+    for ($round = 0; $round < 3; $round += 1) {
         $questionOperator = $operators[array_rand($operators)];
         $questionNumber1 = rand(1, 99);
         $questionNumber2 = rand(1, 99);
-
+    
         if ($questionOperator === '+') {
             $correctAnswer = $questionNumber1 + $questionNumber2;
         } elseif ($questionOperator === '-') {
@@ -27,19 +26,9 @@ function braincalc()
         } else {
             $correctAnswer = $questionNumber1 * $questionNumber2;
         }
-
-        line("Question: {$questionNumber1} {$questionOperator} {$questionNumber2}");
-        $userAnswer = prompt("Your answer");
-        if ($correctAnswer === (int) $userAnswer) {
-            line('Correct!');
-            $winnedRounds += 1;
-        } else {
-            line("'{$userAnswer}' is wrong answer ;(. Correct answer was '{$correctAnswer}'.");
-            line("Let's try again, {$userName}!");
-            break;
-        }
+    
+        $question = "{$questionNumber1} {$questionOperator} {$questionNumber2}";
+        $gameSet[] = [$question, $correctAnswer];
     }
-    if ($winnedRounds === 3) {
-        line("Congratulations, {$userName}!");
-    }
+    return $gameSet;
 }
